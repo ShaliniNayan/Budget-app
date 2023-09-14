b9bde356857b69ac1fd0fe5a4b6572c842593764
Shalini Nayan <117961613+ShaliniNayan@users.noreply.github.com>
2023-09-14 21:50:30 +0530

    Install rspec and add test files in spec folder

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,4 +2,6 @@ class Group < ApplicationRecord
   mount_uploader :icon, IconUploader
   has_many :bills, dependent: :destroy
   belongs_to :user
+
+  validates :name, presence: true
 end
---
+++ spec/models/group_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe Bill, type: :model do
+  let(:user) { User.create(name: 'user', email: 'example@mail.com', password: 'password') }
+  let(:group) do
+    Group.create(name: 'Group', icon: 'icon.png', description: 'description', created_at: Time.now,
+                 updated_at: Time.now, user_id: user.id)
+  end
+
+  subject do
+    described_class.new(name: 'name', amount: 20, author_id: user.id, group_id: group.id)
+  end
+
+  describe 'Validations' do
+    it 'is valid with a name and an amount' do
+      expect(subject).to be_valid
+    end
+
+    it 'is not valid without a name' do
+      subject.name = nil
+      expect(subject).to_not be_valid
+    end
+  end
+end
